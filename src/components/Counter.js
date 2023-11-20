8b65b8f80acbccf5421135d66d591af2dec252f8
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2023-11-20 17:55:40 +0530

    Implemented button action using useDispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,26 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
   const toggleCounterHandler = () => {};
+
+  const incrementHandle = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandle = () => {
+    dispatch({ type: "decrement" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandle}>Increment</button>
+        <button onClick={decrementHandle}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
